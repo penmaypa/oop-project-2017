19eb6281ca72a33ca705bccf33d51f2a55d31e7a
Penuel Maypa <penmayp@gmail.com>
2017-11-22 10:07:35 +0000

    update whatever

---
+++ Github/Java Project/BookingApp/src/bookingapp/Booking.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookingapp;
+
+/**
+ *
+ * @author x16382003
+ */
+public class Booking {
+    
+    public void departure(){
+       
+    }
+    
+    
+}
