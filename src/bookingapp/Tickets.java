141b121e304234b74d8dfa0b7c3cd1c0c81b4d1f
Penuel Maypa <penmayp@gmail.com>
2017-12-04 12:25:38 +0000

    04 Dec 2017 12:25:37

--- src/bookingapp/Tickets.java
+++ src/bookingapp/Tickets.java
@@ -11,44 +11,13 @@ package bookingapp;
  * @author x16333223
  */
 public class Tickets extends Booking{
-   protected String departure;
-   protected String destination;
+  
    Booking myBooking = new Booking();
-   protected double totalPrice;
-   protected double totalPrice1;
+   
    
     public Tickets(){
-        totalPrice1 = 123.00;
+       
      }
    
-    //
-
-    public String getDeparture() {
-        return departure ;
-    }
-
-    public void setDeparture(String departure) {
-        this.departure = departure;
-    }
-
-    public String getDestination() {
-        return destination ;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-   /* 
-    public double getTotalPrice() {
-        return totalPrice = totalPrice + myBooking.totalPrice;
-    }
-    */
-
-    public double getTotalPrice() {
-        return totalPrice = myBooking.getTotalPrice();
-    }
-
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
+    
 }
